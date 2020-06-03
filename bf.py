418880a2e18789140f2d6546f78f225889ead232
Josías Alvarado Gutiérrez <josiasjag@gmail.com>
2020-06-03 16:23:09 -0600

    Update bf.py

--- bf.py
+++ bf.py
@@ -1,5 +1,5 @@
 # bf.py is a Brainfuck interpreter
-# Copyright (C) 2020  Josias Alvarado 
+# Copyright (C) 2020  Josias Alvarado
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -13,92 +13,62 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import pdb
 import sys
 import getch
 
 def execute(filename):
-  with open(filename, 'r') as file:
-      evaluate(file.read())
+    with open(filename, 'r') as file:
+        evaluate(file.read())
 
 def build_loop_map(code):
-  
-  temp_bracestack, bracemap = [], {}
-
-  for position, command in enumerate(code):
-    if command == "[":
-        temp_bracestack.append(position)
-    if command == "]":
-        start = temp_bracestack.pop()
-        bracemap[start] = position
-        bracemap[position] = start
-  return bracemap
+    # A list and a dictionary
+    start_loop, loop_map = [], {}
+    # Now we loop over the string `code` | var position contains the index and command the actual character
+    for position, command in enumerate(code):
+        if command == "[":
+            start_loop.append(position)
+        if command == "]":
+            start = start_loop.pop()
+            loop_map[start] = position
+            loop_map[position] = start
+    return loop_map
 
 def evaluate(code):
-    # array = [0][0][0][0][0]
-    # >>++[<<+]
-
-    bf_code   = cleanup(list(code))
-    loop_map = build_loop_map(bf_code)
-
-    cells     = [0] #cells
-    cellptr   = 0   #cellptr
-    codeptr   = 0   #codeptr
-
-    # +[-[<<[+[--->]-[<<<]]]>>>-]>-.---.>..>.<<<<-.<+.>>>>>.>.<<.<-.
-
-    #pdb.set_trace()
-    while codeptr < len(bf_code):
-        command = bf_code[codeptr]
-
-        if command == '>':
+    code = cleanup(list(code))
+    loop_map = build_loop_map(code)
+    
+    cells = [0]
+    codeptr = 0
+    cellptr = 0
+    
+    while codeptr < len(code):
+        command = code[codeptr]
+        
+        if command == ">":
             cellptr += 1
-            if cellptr == len(cells):
-                cells.append(0)
-        
-        if command == '<':
-            if cellptr <= 0:
-                cellptr = 0
-            else:
-                cellptr - 1
-        
-        if command == '+':
-            cells[cellptr] = cells[cellptr] + 1
-        
-        if command == '-':
-            if cells[cellptr] > 0:
-                cells[cellptr] = cells[cellptr] - 1
-            else:
-                cells[cellptr] = 255
-        
-        if command == '.':
-            sys.stdout.write(chr(cells[cellptr]))
-        
-        if command == ',':
-            cells[cellptr] = ord(getch.getch())
-        
-        #if command == '[' and cells[cellptr] == 0:
-        #    codeptr = loop_map[codeptr]
-
-        #if command == ']' and cells[cellptr] != 0:
-        #    codeptr = loop_map[codeptr]
-
-        if command == '[':
-            codeptr = loop_map[codeptr]
-        
-        if command == ']':
-            codeptr = loop_map[codeptr]
+        if cellptr == len(cells):
+            cells.append(0)
+        if command == "<":
+            cellptr = 0 if cellptr <= 0 else cellptr - 1
+        if command == "+":
+            cells[cellptr] = cells[cellptr] + 1 if cells[cellptr] < 255 else 0
+        if command == "-":
+            cells[cellptr] = cells[cellptr] - 1 if cells[cellptr] > 0 else 255
+        if command == "[" and cells[cellptr] == 0: codeptr = loop_map[codeptr]
+        if command == "]" and cells[cellptr] != 0: codeptr = loop_map[codeptr]
+        if command == ".": sys.stdout.write(chr(cells[cellptr]))
+        if command == ",": cells[cellptr] = ord(getch.getch())
+      
         codeptr += 1
-        #print(codeptr)
 
 def cleanup(code):
     return ''.join(filter(lambda x: x in ['.', ',', '[', ']', '<', '>', '+', '-'], code))
-  
+
 def main():
     if len(sys.argv) == 2:
-      execute(sys.argv[1])
+        execute(sys.argv[1])
     else:
-      print("Usage:", sys.argv[0], "filename")
+        print("Usage:", sys.argv[0], "file.bf")
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     main()
